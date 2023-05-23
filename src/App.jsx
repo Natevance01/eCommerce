d28a6fc58847b1a6cfb245a07c71282a69388250
Nathan Vance <Natevance01@gmail.com>
2023-05-22 22:21:15 -0500

    created authentication component and sign in form

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Home from './routes/home/home.component.jsx'
-import SignIn from './routes/sign-in/signIn.component.jsx'
+import Authentication from './routes/authentication/authentication.component.jsx'
 import { Routes, Route } from 'react-router-dom'
 import Navigation from './components/navigation/navigation.component.jsx'
 
@@ -22,7 +22,7 @@ const App = () => {
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
-        <Route path='sign-in' element={<SignIn />} />
+        <Route path='auth' element={<Authentication />} />
       </Route>
     </Routes>
   )
---
+++ src/routes/authentication/authentication.component.jsx
@@ -0,0 +1,22 @@
+
+
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component.jsx'
+import SignInForm from '../../components/sign-in-form/sign-in-form.component.jsx'
+
+import './authentication.styles.scss'
+
+const Authentication = () => {
+
+
+
+  return (
+     <div className='auth-container'>
+      <SignInForm />
+     
+      <SignUpForm />
+    </div>
+
+  )
+}
+
+export default Authentication
